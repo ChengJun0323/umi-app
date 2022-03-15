23a0bcb0ab7292f4d72a16c5b2c2f5b437c9f711
ChengJun <wsyxmzxhpk@outlook.com>
2022-03-15 22:22:07 +0800

    初始化配置

---
+++ typings.d.ts
@@ -0,0 +1,11 @@
+declare module '*.css'
+declare module '*.less'
+declare module '*.png'
+declare module '*.svg' {
+  export function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement
+  const url: string
+  export default url
+}
+// 声明当前的环境
+declare const ENV: ''
+declare const BASE_URL: ''
